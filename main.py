4a4477321f3e55056187d08fe0b2fa038207d2b3
katyssha <118836572+katyssha@users.noreply.github.com>
2023-04-13 14:12:12 +0300

    24 hw

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import os
-from utils import tools
+from typing import Any, Optional, Union, Iterator, Generator
 
-from flask import Flask, request, jsonify
+from utils import commands
 
-app = Flask(__name__)
+from flask import Flask, request, jsonify, abort, Response
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "data")
+app: Flask = Flask(__name__)
+
+BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR: str = os.path.join(BASE_DIR, "data")
 
 
 @app.route("/perform_query", methods=["POST"])
-def perform_query():
-    data = request.json
-    with open(os.path.join(DATA_DIR, data["file_name"]), "r", encoding="utf-8") as file:
-        info_logs = file.readlines()
-    result1 = tools(file=info_logs, cmd=data["cmd1"], value=data["value1"])
-    result2 = tools(file=result1, cmd=data["cmd2"], value=data["value2"])
-    return jsonify(result2)
+def perform_query() -> Response:
+    data: Optional[Any] = request.json
+    if not data:
+        return jsonify('No data')
+    cmd1: Optional[str] = data.get('cmd1')
+    cmd2: Optional[str] = data.get('cmd2')
+    value1: Optional[str] = data.get('value1')
+    value2: Optional[str] = data.get('value2')
+    filename: Optional[str] = data.get('filename')
+
+    if not filename or not cmd1 or not value1:
+        abort(400, 'Not enough arguments')
+    filepath: str = os.path.join(DATA_DIR, filename)
+    if not os.path.exists(filepath):
+        abort(400, 'There is no such file')
+
+    with open(filename, "r", encoding="utf-8") as file:
+        result: Union[str, Iterator, Generator] = commands(it=file, cmd=data["cmd1"], value=data["value1"])
+        if cmd2 and value2:
+            result2: Union[str, Iterator, Generator] = commands(it=result, cmd=data["cmd2"], value=data["value2"])
+            return jsonify(result2)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,11 +1,27 @@
-def tools(file, cmd, value):
+import re
+from typing import Union, TextIO, Iterator, Generator
+
+def slice_limit(limit: int, it: Union[str, TextIO, Iterator, Generator]) -> Generator:
+    i = 0
+    for item in it:
+        if i < limit:
+            yield item
+        else:
+            break
+        i += 1
+
+def commands(it: Union[str, TextIO, Iterator, Generator], cmd: str, value: str) -> Union[str, Iterator, Generator]:
     if cmd == 'filter':
-        return list(filter(lambda line: value in line, file))
+        return filter(lambda line: value in line, it)
     elif cmd == 'map':
-        return list(map(lambda line: line.split()[int(value)], file))
+        return map(lambda line: line.split()[int(value)], it)
     elif cmd == 'unique':
-        return iter(set(file))
+        return iter(set(it))
     elif cmd == 'limit':
-        return file[:int(value)]
+        return slice_limit(int(value), it)
     elif cmd == 'sort':
-        return sorted(file, reverse=(value == 'desc'))
+        return iter(sorted(it, reverse=value == 'desc'))
+    elif cmd == 'regex':
+        regexp: re.Pattern = re.compile(value)
+        return filter(lambda v: regexp.findall(v), it)
+    return ''
